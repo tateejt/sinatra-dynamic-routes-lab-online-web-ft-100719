aa3f273841e1f19aaebbf91868bc77e510cd79bd
Emery Tate <tateejt@gmail.com>
2019-12-02 03:13:24 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -27,7 +27,7 @@ class App < Sinatra::Base
     number1 = params[:number1].to_i
     number2 = params[:number2].to_i
     
-    answer = 'answer'
+    answer = 'Unable to preform this '
     
     
   end
